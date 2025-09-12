60a6161a8c9466cfc1f67f03f2b927a248068afc
Christian Yarwood <christianyarwood@gmail.com>
2025-09-12 19:34:47 +0100

    Create a static StartScreen component

---
+++ src/StartScreen.jsx
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <div>
+      <h2>Welcome to the quiz!</h2>
+      <h3>X question to test your skills</h3>
+      <button>Lets start</button>
+    </div>
+  );
+}
