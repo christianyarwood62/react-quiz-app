c8b0ef372b6d1a69324e24ad561b32c4ef7affee
Christian Yarwood <christianyarwood@gmail.com>
2025-09-12 19:25:20 +0100

    Add a questions API and a dataReceived state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
-import { useState } from "react";
-import DateCounter from "./DateCounter";
+import { useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main-comp";
+import { useEffect } from "react";
+
+const initialState = {
+  questions: [],
+
+  // list of states: 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
