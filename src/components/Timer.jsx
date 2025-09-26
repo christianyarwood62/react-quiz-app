eccc66ca0ceff7d2f7094916e1ecb3307bd2d063
Christian Yarwood <christianyarwood@gmail.com>
2025-09-26 10:17:35 +0100

    Add 0's to timer when less than 10 secs/mins

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(
     function () {
       const id = setInterval(function () {
+        // Need to add a variable to this to be able to use a cleanup function below
         dispatch({ type: "tick" });
       }, 1000);
 
-      return () => clearInterval(id);
+      return () => clearInterval(id); // Need this cleanup function so it doesnt add more tick useEffects
     },
     [dispatch]
   );
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
